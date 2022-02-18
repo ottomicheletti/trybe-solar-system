690d6314dfb07229dea385aef22831935a3a34af
ottomicheletti <michelettiotto@gmail.com>
2022-02-18 15:52:31 -0300

    Requisito 4

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -5,7 +5,7 @@ class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline={ headline } />
+        <Title headline="Planetas" />
       </div>
     );
   }
